d1858e3b714e3d18f56d953e7a90e47007098ed0
Denis <hahocok@gmail.com>
2020-10-22 20:08:19 +0300

    внес изменения по комментариям

--- lesson5/Main.java
+++ lesson5/Main.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class Main {
     static final int SIZE = 10000000;
-    static final int h = SIZE / 2;
+    static final int H = SIZE / 2;
 
     public static void main(String[] args) {
         method1();
@@ -18,7 +18,7 @@ public class Main {
         long timerStart = System.currentTimeMillis();
 
         for (int i = 0; i < arr.length; i++) {
-            arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+            arr[i] = calcValue(arr[i], i);
         }
 
         final long timerEnd = System.currentTimeMillis();
@@ -27,20 +27,20 @@ public class Main {
 
     private static void method2() {
         float[] arr = new float[SIZE];
-        float[] a1 = new float[h];
-        float[] a2 = new float[h];
+        float[] a1 = new float[H];
+        float[] a2 = new float[H];
         Arrays.fill(arr, 1f);
 
         long timerStart = System.currentTimeMillis();
 
-        System.arraycopy(arr, 0, a1, 0, h);
-        System.arraycopy(a2, 0, arr, h, h);
+        System.arraycopy(arr, 0, a1, 0, H);
+        System.arraycopy(a2, 0, arr, H, H);
 
         new Thread() {
             @Override
             public void run() {
                 for (int i = 0; i < a1.length; ++i) {
-                    a1[i] = (float)(a1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+                    a1[i] = calcValue(a1[i], i);
                 }
             }
         };
@@ -49,17 +49,19 @@ public class Main {
             @Override
             public void run() {
                 for (int i = 0; i < a2.length; ++i) {
-                    a2[i] = (float)(a2[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+                    a2[i] = calcValue(a2[i], i);
                 }
             }
         };
 
-        System.arraycopy(a1, 0, arr, 0, h);
-        System.arraycopy(a2, 0, arr, h, h);
+        System.arraycopy(a1, 0, arr, 0, H);
+        System.arraycopy(a2, 0, arr, H, H);
 
         final long timerEnd = System.currentTimeMillis();
-        System.out.println("calc time method2 = " + (timerEnd - timerStart) + "ms");
+        System.out.print("calc time method2 = " + (timerEnd - timerStart) + "ms");
     }
 
-
+    private static float calcValue(float value, int i) {
+        return  (float)(value * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+    }
 }
